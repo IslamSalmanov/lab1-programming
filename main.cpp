8e07fb68787138be68a067b9f4daef880c7ba1ce
IslamSalmanov <salmanovislam70@mail.ru>
2026-09-24 18:47:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+#include <string> 
+int main() { 
+ std::cout << "Hello, World!" << std::endl;
+ std::string name = "Ислам Сальманов"; 
+ std::cout << "Студент: " << name << std::endl; 
+ std::cout << "Дата: 2026-09-10" << std::endl; 
+ return 0; 
+}
---
+++ main.py
@@ -0,0 +1,4 @@
+print('Hello, World')
+name='Ислам Сальманов'
+print(f'Студент: {name}')
+print('Дата: 2026-09-10')
